8a83a2b8d0df90ef1ca9ba2a9865be0ce6c3a63b
youssefshibl <youssefshibl000@gmail.com>
2023-09-24 19:51:51 +0300

    change

--- test.js
+++
@@ -1,7 +0,0 @@
-// check if file exist in directory
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join(__dirname, 'test.txt');
-if (fs.existsSync(filePath)) {
-  console.log('File exists');
-}   
