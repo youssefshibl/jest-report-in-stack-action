364a5106412f31a63b0609780bee11c3b95644a7
youssefshibl <youssefshibl000@gmail.com>
2023-09-24 21:13:58 +0300

    change

--- action.js
+++ action.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
+const { WebClient, LogLevel } = require("@slack/web-api");
 const fs = require("fs");
 const path = require("path");
 // const filePath = path.join(__dirname,"..","..", "test-report.json");
@@ -14,8 +15,21 @@ async function run() {
     await exec.exec("npm test");
     core.endGroup();
 
-    await exec.exec(`cat ./test-report.json`);
 
+    core.startGroup("Start upload testing file to slack");
+    const SlackToken = core.getInput('slack_token', { required: true });
+    const channelId = core.getInput('channel_id', { required: true });
+
+    const client = new WebClient(
+        SlackToken
+    );
+    await client.files.upload({
+      channels: channelId,
+      initial_comment: "testing file from github repo",
+      file: fs.createReadStream("./test-report.json"),
+    });
+    
+    core.endGroup();
   } catch (error) {
     core.setFailed(error.message);
   }
