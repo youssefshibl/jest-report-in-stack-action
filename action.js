227a1d20ef321f92353d16210de8da5f0b2cb115
youssefshibl <youssefshibl000@gmail.com>
2023-09-24 19:03:24 +0300

    change

--- action.js
+++ action.js
@@ -5,6 +5,8 @@ async function run() {
   try {
     const src = __dirname
     await exec.exec('npm', ['install']);
+    // start testing 
+    core.setOutput('start testing', "start testing")
     await exec.exec('npm test')
     await exec.exec('cat test-report.json')
     
