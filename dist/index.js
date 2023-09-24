131e31e252798e169bcaf0c3a8a7f024e5562c6b
youssefshibl <youssefshibl000@gmail.com>
2023-09-24 19:08:45 +0300

    change

--- dist/index.js
+++ dist/index.js
@@ -4111,14 +4111,13 @@ const exec = __nccwpck_require__(514);
 
 async function run() {
   try {
-    const src = __dirname
-    await exec.exec('npm', ['install']);
-    // start testing 
-    core.setOutput('start testing', "start testing")
-    await exec.exec('npm test')
-    await exec.exec('cat test-report.json')
-    
-
+    const src = __dirname;
+    await exec.exec("npm", ["install"]);
+    // start testing
+    core.startGroup("start testing ");
+    await exec.exec("npm test");
+    await exec.exec("cat test-report.json");
+    core.endGroup();
   } catch (error) {
     core.setFailed(error.message);
   }
